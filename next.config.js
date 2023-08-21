686d7ebc54e12d6de7250e5bf0cdf6a1a755b98f
viparosh <protectedstaticfunction@gmail.com>
2023-08-22 06:39:17 +0800

    haram

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: '@svgr/webpack',
+    })
+    config.resolve.fallback = { fs: false }
+
+    return config
+  },
+  serverRuntimeConfig: {
+    maxRequestBodySize: '4mb',
+  },
+  //reactStrictMode: false,
+  //webpack5: true,
+  optimizeFonts: false,
 }
